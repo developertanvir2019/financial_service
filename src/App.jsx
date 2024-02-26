0f1497f177e1d17481c416bf9cdca6e167e8bcc1
developertanvir2019 <developertanvir2019@gmail.com>
2024-02-27 02:41:20 +0600

    added login with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { router } from "./routes/router";
 
 function App() {
   return (
-    <>
-      <div>
+    <div className="flex justify-center">
+      <div style={{ maxWidth: "1280px" }} className="w-full bg-[#EBECF0]">
         <RouterProvider router={router}></RouterProvider>
       </div>
-    </>
+    </div>
   );
 }
 
