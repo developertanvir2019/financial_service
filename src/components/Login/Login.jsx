eb228ffb0ceab732b0fbad3505e268cb271247c0
developertanvir2019 <developertanvir2019@gmail.com>
2024-02-27 02:06:27 +0600

    added register signup gape

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,139 @@
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { AuthContext } from "../components/shared/ValueProvider/AuthProvider";
+
+const Login = () => {
+  const { setIsLoggedIn } = useContext(AuthContext);
+  const [showPass, setShowPass] = useState(true);
+  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("https://city-server-cwdm.onrender.com/api/user/login", loginData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        console.log(data.token);
+        if (data?.token) {
+          localStorage.setItem("jwtToken", data.token);
+          setIsLoggedIn(true);
+          Swal.fire("Congratulations!!!", "Log in success", "success");
+          navigate("/");
+          window.location.reload();
+        } else {
+          Swal.fire("Login Failed", "Number or password is incorrect", "error");
+        }
+      })
+      .catch((error) => {
+        Swal.fire("Login Failed", error.response.data.error, "error");
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  return (
+    <div className="flex justify-center">
+      <form
+        onSubmit={handleLoginSubmit}
+        className=" mt-10 sm:w-72 md:w-96 lg:w-96 overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200"
+      >
+        {/*  <!-- Body--> */}
+        <div className="p-6">
+          <header className="mb-4 text-center">
+            <h3 className="text-2xl font-medium text-slate-700 ">Login</h3>
+          </header>
+          <div className="flex flex-col space-y-8">
+            {/*      <!-- Input field --> */}
+            <div className="relative mt-6">
+              <input
+                onChange={handleInputChange}
+                required
+                id="email"
+                type="email"
+                name="email"
+                placeholder="your name"
+                className="peer relative h-10 bg-white w-full rounded border border-slate-200 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+              />
+              <label
+                htmlFor="email"
+                className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+              >
+                Your email
+              </label>
+            </div>
+            {/*      <!-- Input field --> */}
+            <div className="relative my-6">
+              <input
+                onChange={handleInputChange}
+                required
+                id="password"
+                type={showPass ? "password" : "text"}
+                name="password"
+                placeholder="your password"
+                className="peer relative h-10 bg-white w-full rounded border border-slate-200 px-4 pr-12 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+              />
+              <label
+                htmlFor="password"
+                className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+              >
+                Your password
+              </label>
+
+              <svg
+                onClick={() => setShowPass(!showPass)}
+                xmlns="http://www.w3.org/2000/svg"
+                className="absolute top-2.5 right-4 h-5 w-5 cursor-pointer stroke-slate-400 peer-disabled:cursor-not-allowed"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                />
+              </svg>
+            </div>
+          </div>
+        </div>
+        {/*  <!-- Action base sized basic button --> */}
+        <div className="flex justify-end pb-6 px-6">
+          <button
+            type="submit"
+            className="inline-flex h-10 w-full items-center justify-center gap-2 whitespace-nowrap rounded bg-primary px-5 text-sm font-medium tracking-wide text-white transition duration-300 hover:bg-emerald-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none"
+          >
+            <span>Log in</span>
+          </button>
+        </div>
+        <div className="px-5 pb-5">
+          <p className="text-gray-500">
+            Sign up for a free account at our store.
+          </p>
+          <Link to="/register">
+            <span className="underline font-semibold text-primary">
+              create account
+            </span>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/routes/router.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../components/Error/Error";
+import Main from "../layout/Main";
+import Home from "../components/Home/Home";
+import Login from "../components/Login/Login";
+import Register from "../components/Register/Register";
+
+export const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <Error />,
+  },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Register />,
+      },
+    ],
+  },
+]);
