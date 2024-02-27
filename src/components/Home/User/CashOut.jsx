37d06690653490a6f054c738a5d62aa54584fffa
developertanvir2019 <developertanvir2019@gmail.com>
2024-02-28 01:37:02 +0600

    added password for cash out

--- src/components/Home/User/CashOut.jsx
+++ src/components/Home/User/CashOut.jsx
@@ -5,6 +5,7 @@ import Swal from "sweetalert2";
 
 const CashOut = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [showPass, setShowPass] = useState(true);
   const openModal = () => {
     setModalOpen(true);
   };
@@ -14,6 +15,7 @@ const CashOut = () => {
     receiverPhone: "",
     senderPhone: "",
     amount: "",
+    password: "",
   });
   useEffect(() => {
     const token = localStorage.getItem("jwtToken");
@@ -149,6 +151,39 @@ const CashOut = () => {
                       >
                         amount
                       </label>
+                    </div>
+                    <div className="relative my-2">
+                      <input
+                        required
+                        onChange={handleInputChange}
+                        id="password"
+                        type={showPass ? "password" : "text"}
+                        name="password"
+                        placeholder="your password"
+                        className="peer relative h-10 bg-transparent w-full rounded border border-slate-200 px-4 pr-12 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-emerald-500 focus:outline-none invalid:focus:border-pink-500 disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                      />
+                      <label
+                        htmlFor="password"
+                        className="absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                      >
+                        Your password
+                      </label>
+
+                      <svg
+                        onClick={() => setShowPass(!showPass)}
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute top-2.5 right-4 h-5 w-5 cursor-pointer stroke-slate-400 peer-disabled:cursor-not-allowed"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                        />
+                      </svg>
                     </div>
                   </div>
                   {/*  <!-- Action base sized basic button --> */}
--- src/components/Shared/Header.jsx
+++ src/components/Shared/Header.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [activeNav, setActiveNav] = useState("/");
+  const handleLogout = () => {
+    localStorage.removeItem("jwtToken");
+    window.location.href = "/login";
+  };
   return (
-    <div className="bg-secondary text-white px-12 py-4">
-      <div>
+    <div className="bg-secondary text-white px-3 md:px-12 py-4 sticky top-0">
+      <div className="flex justify-between font-semibold">
         <h2 className="text-2xl font-bold">ProCash</h2>
+        <div className="flex items-center gap-3 text-white">
+          <Link
+            className={activeNav === "/" && "text-gray-300"}
+            onClick={() => setActiveNav("/")}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className={activeNav === "/users" && "text-gray-300"}
+            onClick={() => setActiveNav("/users")}
+            to="/users"
+          >
+            Users
+          </Link>
+        </div>
+        <button onClick={() => handleLogout()} className="px-3 py-2 bg-primary">
+          Logout
+        </button>
       </div>
     </div>
   );
